51b12fba7c18c0007a98920139eee4cdf303c467
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-12-18 10:46:49 -0700

    complete linearSearch

--- searching.js
+++ searching.js
@@ -1,7 +1,11 @@
 // Implement a linear search algorithm
 
 function linearSearch(array, target) {
-
+  for (var i=0; i<array.length; i++) {
+    if (array[i] == target) {
+      return i;
+    }
+  } return -1;
 }
 
 // Implement a binary search algorithm
@@ -9,3 +13,8 @@ function linearSearch(array, target) {
 function binarySearch(array, target) {
   
 }
+
+const data = [3, 1, 12, 5, 4, 8, 22]
+const sorted = [1, 4, 12, 15, 21, 28, 32]
+
+console.log(linearSearch(data, 12));
